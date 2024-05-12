0395c89d258c94aa99f2fe430e40d806753ca044
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-05-12 14:26:52 +0200

    first commit

---
+++ src/test/java/com/orcone/wow/WowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orcone.wow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
